8ad60bd388ccc3c289e9b51ff1443c3cae8dccf6
Muhammad Ismail <iamismail91@gmail.com>
2022-03-19 23:34:15 +0500

    Icon deteted

--- ForecastTask/Network/NetworkRequestInterceptor.swift
+++ ForecastTask/Network/NetworkRequestInterceptor.swift
@@ -20,9 +20,6 @@ class NetworkRequestInterceptor: RequestInterceptor {
     
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         let urlRequest = urlRequest
-        /* if let token = TokenManager.shared.fetchAccessToken() {
-         urlRequest.setValue("token \(token)", forHTTPHeaderField: "Authorization")
-         } */
         completion(.success(urlRequest))
     }
     
